143f6c568c2b62ace19b958c5c3b48499c7768e5
GABRIEL BARBOSA <gabriellb438@outlook.com>
2021-02-04 19:27:08 -0300

    Sitemap

--- master.php
+++ master.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <title>Indesfer</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+    <meta content="indesfer;ferro;aço," name="keywords">
+    <meta name="description" content="A description of the page" />
+    <meta name="google-site-verification" content="..." />
 
     <!-- Favicons -->
     <link href="img/icon.svg" rel="icon">
