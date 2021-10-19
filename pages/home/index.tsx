a80aa005748663f9edf61e59bf664e39ed16f51d
abnerndr <abnerndr@gmail.com>
2021-10-18 23:52:10 -0300

    update

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -4,22 +4,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import React, { useState,useEffect,useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Navbar from '../../components/navbar/navbar'
 import style from '../home/style.module.css'
 
-import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation"
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 // import Swiper core and required modules
-import SwiperCore, {
-  Navigation
-} from 'swiper';
+import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper'
 
 // install Swiper modules
-SwiperCore.use([Navigation]);
+SwiperCore.use([Autoplay, Pagination, Navigation])
 
 const Home: NextPage = () => {
 	const [shownav, setShowNav] = useState(false)
@@ -70,86 +71,124 @@ const Home: NextPage = () => {
 			<body className={style.body}>
 				{/*NAVBAR*/}
 				<nav className={style.nav}>
-					{shownav ? 
-					<Navbar
-					onClick={onClick}
-					/> :<>
-						<div className={style.center}>
-							<button onClick={onClick}>
-								<img src="arrow.png" alt="Arrow" height="30" />
-							</button>
-							<img src="logo.webp" alt="Indesfer Logo" />
-						</div>
+					{shownav ? (
+						<Navbar onClick={onClick} />
+					) : (
+						<>
+							<div className={style.center}>
+								<button onClick={onClick}>
+									<img
+										src="arrow.png"
+										alt="Arrow"
+										height="30"
+									/>
+								</button>
+								<img src="logo.webp" alt="Indesfer Logo" />
+							</div>
 						</>
-					}
+					)}
 				</nav>
 				<main>
-					<Swiper navigation={true} className={style.swiper}>
+					<Swiper
+						spaceBetween={30}
+						centeredSlides={true}
+						autoplay={{
+							delay: 10000,
+							disableOnInteraction: false,
+						}}
+						pagination={{
+							clickable: true,
+						}}
+						navigation={true}
+						className="mySwiper"
+					>
 						<SwiperSlide>
 							<img
 								className={style.imagemFundo}
 								src="/001_112338.webp"
 								alt="Picture of the author"
 							/>
+							<div className={style.txtslide}>
+								<h2>VIGAS ENRIJECIDAS</h2>
+								<p>
+									As construções feitas com perfil U
+									enrijecido, em comparação a outros
+									materiais, oferecem redução de peso de até
+									1/3 das estruturas
+								</p>
+							</div>
 						</SwiperSlide>
 						<SwiperSlide>
 							<img
 								className={style.imagemFundo}
-								src="/001_112338.webp"
+								src="/002_112425.webp"
 								alt="Picture of the author"
 							/>
+							<div className={style.txtslide}>
+								<h2>METALONS</h2>
+								<p>
+									O metalon é um tubo de aço carbono
+									largamente utilizado na construção civil.
+								</p>
+							</div>
 						</SwiperSlide>
 						<SwiperSlide>
 							<img
 								className={style.imagemFundo}
-								src="/001_112338.webp"
+								src="/004_112530.webp"
 								alt="Picture of the author"
 							/>
+							<div className={style.txtslide}>
+								<h2>CANTONEIRAS</h2>
+								<p>
+									Cantoneiras para todos as necessidades você
+									encontra aqui na Indesfer.
+								</p>
+							</div>
 						</SwiperSlide>
 						<SwiperSlide>
 							<img
 								className={style.imagemFundo}
-								src="/001_112338.webp"
+								src="/012_113543.webp"
 								alt="Picture of the author"
 							/>
-						</SwiperSlide>
-						<SwiperSlide>
-							<img
-								className={style.imagemFundo}
-								src="/001_112338.webp"
-								alt="Picture of the author"
-							/>
-						</SwiperSlide>
-						<SwiperSlide>
-							<img
-								className={style.imagemFundo}
-								src="/001_112338.webp"
-								alt="Picture of the author"
-							/>
-						</SwiperSlide>
-						<SwiperSlide>
-							<img
-								className={style.imagemFundo}
-								src="/001_112338.webp"
-								alt="Picture of the author"
-							/>
-						</SwiperSlide>
-						<SwiperSlide>
-							<img
-								className={style.imagemFundo}
-								src="/001_112338.webp"
-								alt="Picture of the author"
-							/>
-						</SwiperSlide>
-						<SwiperSlide>
-							<img
-								className={style.imagemFundo}
-								src="/001_112338.webp"
-								alt="Picture of the author"
-							/>
+							<div className={style.txtslide}>
+								<h2>FERROS CHATOS</h2>
+								<p>
+									Toda a diversidade de ferros chatos você
+									encontra aqui na Indesfer.
+								</p>
+							</div>
 						</SwiperSlide>
 					</Swiper>
 				</main>
+				<section>
+					<div className={style.sobretitle}>
+						<h2>SOBRE</h2>
+					</div>
+					<div className={style.bg}>
+						<p>
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting industry. Lorem Ipsum has been the
+							industrys standard dummy text ever since the 1500s,
+							when an unknown printer took a galley of type and
+							scrambled it to make a type specimen book. It has
+							survived not only five centuries, but also the leap
+							into electronic typesetting, remaining essentially
+							unchanged. It was popularised in the 1960s with the
+							release of Letraset sheets containing Lorem Ipsum
+							passages, and more recently with desktop publishing
+							software like Aldus PageMaker including versions of
+							Lorem Ipsum.
+						</p>
+					</div>
+				</section>
+
+				<div className={style.catalogo}>
+					<div className={style.title}>
+						<h2>CATÁLOGO</h2>
+					</div>
+				</div>
 			</body>
 		</>
 	)
